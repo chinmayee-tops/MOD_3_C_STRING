046da339d9e3053f38aff968da762891886ae480
chinmayee-tops <151346608+chinmayee-tops@users.noreply.github.com>
2023-12-02 13:28:42 +0530

    Add files via upload

---
+++ 9_Max_Char.c
@@ -0,0 +1,33 @@
+//Write a program in C to find the maximum number of characters in a string.
+
+#include<stdio.h>
+
+main()
+{
+	char str[100], result;
+  	int i, len;
+  	int max=0;
+  	
+  	int freq[256]={0}; 
+ 
+  	printf("\n\n\t Input any String :  ");
+  	gets(str);
+  	
+  	len=strlen(str);
+  	
+  	for(i=0;i<len;i++)
+  	{
+  		freq[str[i]]++;
+	}
+  		
+  	for(i=0; i<256;i++)
+  	{
+		if(freq[i]>freq[max])
+		{
+			max=i;
+		}
+	}
+	printf("\n\n\t Character '%c' appears Maximum of %d Times in a Given String :  %s ", max, freq[max], str);
+	
+	
+}
